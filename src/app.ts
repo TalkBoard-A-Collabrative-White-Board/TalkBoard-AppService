7fbbc29259fd14a76a9d6a3473a7a16cd040be87
Arjun.O <arjundlm10@gmail.com>
2025-12-31 20:10:41 +0530

    Basic Installation Setups

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic route
+app.get("/", (req, res) => {
+  res.send("TalkBoard server running 🚀");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
