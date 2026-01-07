15f97c17c162f2cb8010f846f970ac080a7cf8e7
Arjun.O <arjundlm10@gmail.com>
2026-01-07 21:42:58 +0530

    Added Global Error Handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Response, Request } from 'express';
 import userRoutes from './routes/user.routes.js';
+import { ApiError } from './utils/ApiError.js';
+import { ApiResponse } from './utils/ApiResponse.js';
 
 const app = express();
 
@@ -8,5 +10,17 @@ app.use(express.json());
 
 // Basic route
 app.use('/api/v1/user', userRoutes);
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
+  if (err instanceof ApiError) {
+    return res
+      .status(err.statusCode)
+      .json(new ApiResponse(err.statusCode, null, err.message));
+  }
+  return res
+    .status(500)
+    .json(new ApiResponse(500, err?.message || 'Internal Server Error'));
+});
 
 export { app };
